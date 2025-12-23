7600a3ab854dc2a845c54db87e9a518b52070c47
Fadhla <fadhla153@gmail.com>
2025-12-23 15:31:15 +0700

    V 1.5

--- riwayat_pelanggan.php
+++ riwayat_pelanggan.php
@@ -120,4 +120,8 @@ $q_riwayat = pg_query($conn, "
 
 </div>
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+
 </html>
